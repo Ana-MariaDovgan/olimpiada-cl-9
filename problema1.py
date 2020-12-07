ef126fffcfb87e933fbaa75f8581e25a06cdcd3e
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-12-07 11:05:38 +0200

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -1,3 +1,6 @@
+<<La o ferma avicola sunt x pasari. Jumatate din acestea sunt gaini. Numarul ratelor constituie un sfert din numarul gainilor. Celelalte sunt 
+gaste. Scrieti un program care citeste de la tastatura numarul total de pasari si afiseaza numarul de gaini, rate si gaste.>> 
+
 n=int(input("numarul total de pasari: "))
 g=n//2
 r=g//4
