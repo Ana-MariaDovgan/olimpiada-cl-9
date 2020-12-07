1311ffaaa73e63ed2cd319f2703a521fd657e388
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-12-07 11:07:13 +0200

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -1,3 +1,5 @@
+<<Sa se scrie un program care citeste un numar de ani si calculeaza numarul de luni,zile si ore corespunzatoare. Se considera ca un an are 365 
+zile.>>
 n = int(input("dati numarul de ani: "))
 print("In",n,"ani sunt", n * 12,"luni")
 print("In",n,"ani sunt", n * 52,"saptamani")
