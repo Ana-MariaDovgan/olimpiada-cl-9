aaaebf7d5b6e23cd5de4ca3097c43424be533526
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-12-07 11:03:08 +0200

    Add files via upload

---
+++ problema2.py
@@ -0,0 +1,2 @@
+a=eval(input("Nr. de alune: "))
+print("pentru fiecare purcel revine",a//3,"alune, Ritei i-au ramas",a%3,"alune.")
---
+++ problema4.py
@@ -0,0 +1,7 @@
+perimpat = 48
+latpat = perimpat // 4
+latdr = 9
+ariapat = latpat ** 2
+lundr = ariapat // latdr
+perimdr = (2 * lundr) + (2 * latdr)
+print("Lungimea gardului lotului dreptunghic este de",perimdr,"metri")
