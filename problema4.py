5d786cc87ad20fdcac19b32d97b572c2e389c96c
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-12-07 11:08:57 +0200

    Update problema4.py

--- problema4.py
+++ problema4.py
@@ -1,3 +1,5 @@
+<<Doi gospodari au loturi de aceiasi arie. Un lot are forma patrata si este imprejmuit cu un gard de 48 m lungime. Un alt lot are forma unui 
+dreptunghi cu latimea de 9 m. Ce lungime are gardul care imprejmuieste acest lot?>>
 perimpat = 48
 latpat = perimpat // 4
 latdr = 9
